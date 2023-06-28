4ceafd563b3623da3396477a26ce44016064b60d
Francisco Pedraza Vázquez <101214411+fpedrazav02@users.noreply.github.com>
2023-06-28 17:00:31 +0000

    Jun 28, 2023, 7:00 PM

--- AD/src/Get-ADUserList.ps1
+++ AD/src/Get-ADUserList.ps1
@@ -1,17 +1,17 @@
 <#
 .Synopsis
- Generate diferent Outputs for fast AD User lists and fields. Export in diferent formats.
+    Generate diferent Outputs for fast AD User lists and fields. Export in diferent formats.
 .DESCRIPTION
- Wrapper of Get-ADUser which lets you get a more usefull output as well as easier bulk operations and outputs.
+    Wrapper of Get-ADUser which lets you get a more usefull output as well as easier bulk operations and outputs.
 .EXAMPLE
     # RELLENAR
 .EXAMPLE
-    # RELLENAR
+    # RELLENAR 
 .INPUTS
 
 .NOTES
     You will need the following dependencies:
-        1)
+        1) Test
         2)
 
     Import-Module -Name ".\PowerLibrary\moduleA"
