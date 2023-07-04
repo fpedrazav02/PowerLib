94fc7285a07eafe1c3239590ed48c0fa41eb406c
Francisco Pedraza Vázquez <101214411+fpedrazav02@users.noreply.github.com>
2023-07-04 10:44:01 +0200

    feat:Centerprompt has new color mod for both text & box

--- PowerVisuals/src/CenterPrompt.ps1
+++ PowerVisuals/src/CenterPrompt.ps1
@@ -1,10 +1,12 @@
 function CenterPrompt {
     param (
         [Parameter(Mandatory = $true)]
-        [string[]]$text
+        [string[]]$text,
+        [ValidateSet("Black", "DarkBlue", "DarkGreen", "DarkCyan", "DarkRed", "DarkMagenta", "DarkYellow", "Gray", "DarkGray", "Blue", "Green", "Cyan", "Red", "Magenta", "Yellow", "White")]
+        [string]$colorBox = "Red",
+        [ValidateSet("Black", "DarkBlue", "DarkGreen", "DarkCyan", "DarkRed", "DarkMagenta", "DarkYellow", "Gray", "DarkGray", "Blue", "Green", "Cyan", "Red", "Magenta", "Yellow", "White")]
+        [string]$colorText = "Green"
     )
-    $colorBox = "Red"
-
     $hostWidth = $host.UI.RawUI.BufferSize.Width
     $promptLength = $host.UI.RawUI.CursorPosition.X
     $spacesToCenter = [Math]::Max(0, ($hostWidth - $promptLength) / 2)
@@ -27,7 +29,7 @@ function CenterPrompt {
         $lineStart = '║' + (' ' * $leftPadding)
         $lineEnd = (' ' * $rightPadding) + '║'
         Write-Host $lineStart -NoNewline -ForegroundColor $colorBox
-        Write-Host $line -NoNewline -ForegroundColor Green
+        Write-Host $line -NoNewline -ForegroundColor $colorText
         Write-Host $lineEnd -ForegroundColor $colorBox
     }
     Write-Host $emptyLine -ForegroundColor $colorBox
