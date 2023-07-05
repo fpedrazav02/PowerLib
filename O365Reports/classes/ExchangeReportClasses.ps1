540cc411afae98215e3d16135dc27c72415acac6
Francisco Pedraza Vázquez <101214411+fpedrazav02@users.noreply.github.com>
2023-07-05 11:15:24 +0000

    Jul 5, 2023, 1:15 PM

---
+++ O365Reports/classes/ExchangeReportClasses.ps1
@@ -0,0 +1,40 @@
+Class ExchangeMailbox {
+
+    #Class specifics
+    [Int32]$ID                  # ID enumeration
+    [string]$Mail               # Mailbox UPN
+    [string]$ExchangeGuid       # Mailbox ID
+    [bool]$IsMailboxEnabled     # Boolean is Mailbox is Enabled or disabled
+    [bool]$IsDirSynced          # Cloud or On-premise based resource
+    [string]$AccountType        # Original type of account
+    [string]$MailboxType        # Actual Type of Mailbox
+    [string]$PrimarySmtpAddress # Principal SMTP
+    [string]$Smtps              # All Smtps alias for the desginated Mailbox
+    [string]$CreationDate       # Date of creation
+
+    #Class OverloadMethods
+        #Default OverloadDefinition
+    ExchangeMailbox () {} 
+
+        #Only Mail default OverloadDefinition
+    ExchangeMailbox ([string]$Mail)
+    {
+        $this.Mail = $Mail
+    }
+
+    ExchangeMailbox ([Int32]$ID,[string]$Mail, [string]$ExchangeGuid, [bool]$IsMailboxEnabled, [bool]$IsDirSynced,[string]$PrimarySmtpAddress,[string]$Smtps,[string]$AccountType,[string]$MailboxType,[string]$CreationDate)
+    {
+        $this.ID = $ID
+        $this.Mail = $Mail
+        $this.ExchangeGuid = $ExchangeGuid
+        $this.IsMailboxEnabled = $IsMailboxEnabled
+        $this.IsDirSynced = $IsDirSynced
+        $this.AccountType = $AccountType
+        $this.MailboxType = $MailboxType
+        $this.PrimarySmtpAddress = $PrimarySmtpAddress
+        $this.Smtps = $Smtps
+        $this.CreationDate = $CreationDate
+    }
+
+    #Class Methods
+}
