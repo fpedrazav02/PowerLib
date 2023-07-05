360db5e775a432ff9c24d785d67e92396e6ff42a
Francisco Pedraza Vázquez <101214411+fpedrazav02@users.noreply.github.com>
2023-07-05 11:59:37 +0000

    Jul 5, 2023, 1:59 PM

--- PowerLib.psm1
+++ PowerLib.psm1
@@ -14,7 +14,7 @@ Import specific module:
 
 Import-Module -Name "$PSScriptRoot\AD"  -Force 
 Import-Module -Name "$PSScriptRoot\PowerVisuals"  -Force
-Import-Module -Name "$PSScriptRoot\O365Reports\O365Reports.psd1"  -Force -Verbose
+Import-Module -Name "$($PSScriptRoot)\O365Reports\O365Reports.psd1"  -Force -Verbose
  # Import-Module -Name
 
 Export-ModuleMember -Function * -Cmdlet * -Alias *
