9519e7f495de06f4bdb49adb075cf1ab9a5389cf
Francisco Pedraza Vázquez <101214411+fpedrazav02@users.noreply.github.com>
2023-07-07 11:15:36 +0000

    Jul 7, 2023, 1:15 PM

--- PowerVisuals/src/New-ListItem.ps1
+++ PowerVisuals/src/New-ListItem.ps1
@@ -1,14 +1,15 @@
-function New-ItemList {
+function New-ListItem {
     param (
         [Parameter(Mandatory = $true)]
         [string]$text,
 
         [ValidateSet("Black", "DarkBlue", "DarkGreen", "DarkCyan", "DarkRed", "DarkMagenta", "DarkYellow", "Gray", "DarkGray", "Blue", "Green", "Cyan", "Red", "Magenta", "Yellow", "White")]
-        [string]$colorText = "Green",
-
+        [string]$colorMarkColor = "Cyan",
         [ValidateSet("Black", "DarkBlue", "DarkGreen", "DarkCyan", "DarkRed", "DarkMagenta", "DarkYellow", "Gray", "DarkGray", "Blue", "Green", "Cyan", "Red", "Magenta", "Yellow", "White")]
-        [string]$backgroundColor = "Black"
+        [string]$colorText = "White"
     )
 
     $character = '├─'
+    Write-Host "`e[3m$character`e[0m" -NoNewline -ForegroundColor $colorMarkColor
+    Write-Host "$text" -ForegroundColor $colorText
 }
