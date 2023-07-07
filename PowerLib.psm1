f61d63b9dded7381599a4af88efe1a041591052b
Francisco Pedraza Vázquez <101214411+fpedrazav02@users.noreply.github.com>
2023-07-07 09:23:57 +0000

    Jul 7, 2023, 11:23 AM

--- PowerLib.psm1
+++ PowerLib.psm1
@@ -11,13 +11,12 @@
 Import specific module:
     Import-Module -Name ".\PowerLibrary\moduleA"
 #>
-using module ".\O365Reports\classes\ExchangeReport.Class.psm1"
 $Modules = Get-ChildItem $PSScriptRoot -Directory
 $exceptions = @('.vscode')
 
 foreach ($module in $Modules)
 {
-    if (!($module -in $exceptions))
+    if ($module -notin $exceptions)
     {
         Write-Host $module.FullName
         Import-Module -Name "$($module.FullName)" -Force
@@ -25,6 +24,48 @@ foreach ($module in $Modules)
 }
 
  # Import-Module -Name
+
+ Class ExchangeMailbox {
+
+    #Class specifics
+    [Int32]$ID                  # ID enumeration
+    [string]$Mail               # Mailbox UPN
+    [string]$ExchangeGuid       # Mailbox ID
+    [bool]$IsMailboxEnabled     # Boolean is Mailbox is Enabled or disabled
+    [bool]$IsDirSynced          # Cloud or On-premise based resource
+    [string]$AccountType        # Original type of account
+    [string]$MailboxType        # Actual Type of Mailbox
+    [string]$PrimarySmtpAddress # Principal SMTP
+    [string]$Smtps              # All Smtps alias for the desginated Mailbox
+    [string]$CreationDate       # Date of creation
+
+    #Class OverloadMethods
+        #Default OverloadDefinition
+    ExchangeMailbox () {} 
+
+        #Only Mail default OverloadDefinition
+    ExchangeMailbox ([string]$Mail)
+    {
+        $this.Mail = $Mail
+    }
+
+    ExchangeMailbox ([Int32]$ID,[string]$Mail, [string]$ExchangeGuid, [bool]$IsMailboxEnabled, [bool]$IsDirSynced,[string]$PrimarySmtpAddress,[string]$Smtps,[string]$AccountType,[string]$MailboxType,[string]$CreationDate)
+    {
+        $this.ID = $ID
+        $this.Mail = $Mail
+        $this.ExchangeGuid = $ExchangeGuid
+        $this.IsMailboxEnabled = $IsMailboxEnabled
+        $this.IsDirSynced = $IsDirSynced
+        $this.AccountType = $AccountType
+        $this.MailboxType = $MailboxType
+        $this.PrimarySmtpAddress = $PrimarySmtpAddress
+        $this.Smtps = $Smtps
+        $this.CreationDate = $CreationDate
+    }
+
+    #Class Methods
+}
+
 
 Export-ModuleMember -Function * -Cmdlet * -Alias *
     
